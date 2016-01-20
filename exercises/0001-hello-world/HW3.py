756fbbc52b58eb0bef88a7a5b24c36dd06b78bd8
caitlin kramon <caitlinkramon@DN0a223740.SUNet>
2016-01-20 14:19:30 -0800

    hello

---
+++ exercises/0001-hello-world/HW3.py
@@ -0,0 +1,33 @@
+>>> requests.get("http://www.example.com")
+<Response [200]>
+
+>>> resp = requests.get("http://www.example.com")
+
+>>> type(resp)
+requests.models.Response
+
+>>> resp = requests.get("http://www.example.com")
+>>> print(resp.text)
+<!doctype html>
+<html>
+<head>
+	<title>Example Domain</title>
+
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<style type="text/css">
+
+>>> "Hello world".upper()
+
+>>>'hello world'.upper
+<function str.upper>
+
+>>> a = 42
+>>> a.numerator
+
+
+
+
+
+
