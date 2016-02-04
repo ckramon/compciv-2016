76bf7e73cf5ba1de417debaf2af82649ad2ae5f4
caitlin kramon <caitlinkramon@rescomp-15-299582.stanford.edu>
2016-02-04 01:13:33 -0800

    stuff

--- exercises/0010-map-json-responses/f.py
+++ exercises/0010-map-json-responses/f.py
@@ -11,36 +11,13 @@ f.close()
 
 print("type:", mydict['type'])
 
-for i in mydict['geocoding']:
-    mylist = []
-    mylist.append(i['query'])
-    loc = i['geocoding']['query']
-    mylist.append(str(loc['text']))
-    mylist.append(str(loc['size']))
-    mylist.append(str(loc['boundary.country']))
+query_dict = mydict['geocoding']['query']
 
-print(';'.join(mylist))
-
-# print("text:", mydict['geocoding']['query'])
+print("text:" , query_dict['text'])
+print("size:", query_dict['querySize'])
+print("boundary.country:", query_dict['boundary.country'])
 
 
 
-# for result in mydict['results']:
-#     mylist = []
-#     mylist.append(result['formatted_address'])
-#     loc = result['geometry']['location']
-#     mylist.append(str(loc['lng']))
-#     mylist.append(str(loc['lat']))
 
-# for i in mydict['geocoding']:
-#     mylist = []
-#     mylist.append(str(i['text']))
-#     mylist.append(str(i['size']))
-#     mylist.append(str(i['boundary.country']))
-        
-# print(';'.join(mylist))
-        
-# print("type:", mydict['type'])
-# print("text:" , mydict['text'])
-# print("size:", mydict['querySize'])
-# print("boundary.country:", mydict['boundary.country'])
+
